65dd4d4b99eea87945ff493ba280880856e721de
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2025-03-05 23:06:40 +0100

    first release

--- src/main/java/dev/ferriarnus/tinkersjewelry/ClientEvents.java
+++ src/main/java/dev/ferriarnus/tinkersjewelry/ClientEvents.java
@@ -1,11 +1,12 @@
 package dev.ferriarnus.tinkersjewelry;
 
 import dev.ferriarnus.tinkersjewelry.book.JewerlyBook;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = TinkersJewelry.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = TinkersJewelry.MODID, value = Dist.CLIENT)
 public class ClientEvents {
 
     @SubscribeEvent
